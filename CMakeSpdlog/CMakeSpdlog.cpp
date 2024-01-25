cbaf6613829a20eeeb646aa4bfb3a47bfc1dfbb1
Nigel Morse <musmuris@users.noreply.github.com>
2024-01-25 12:54:14 +0000

    init

---
+++ CMakeSpdlog/CMakeSpdlog.cpp
@@ -0,0 +1,14 @@
+﻿#include "CMakeSpdlog.h"
+
+using namespace std;
+
+#include "spdlog/spdlog.h"
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+	spdlog::info("Hello {}", 42);
+
+	return 0;
+}
